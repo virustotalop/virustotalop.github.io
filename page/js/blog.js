e43a4ecf259c2ce03c4a1e36fbf93da663ad9720
virustotalop <virustotalop@gmail.com>
2021-02-12 04:00:45 -0800

    Make blog posts expandable

--- page/js/blog.js
+++ page/js/blog.js
@@ -1,45 +1,78 @@
+const relative = "blog/page/";
+
 //Load page
 pageRegistrar.register(new Page("blog", () => {
 	//Parse blog
-	$.getJSON("blog/json/blog.json", function(data) {
+	$.getJSON("blog/json/blog.json", (data) => {
 		console.log(data);
-		const relative = "blog/page/";
-		const replaceMap = {
-			'<h1': '<div class="justify-content-center row"><h1 class="section-header"',
-			'</h1>': '</h1></div>',
-			'<p>': '<div class="card-body row">',
-			'</p>': '</div>'
-		};
-		//https://getbootstrap.com/docs/4.0/components/card/#header-and-footer
 
-		const bindings = Object.keys(replaceMap)
-		.map(key => ({
-			type: 'output',
-			regex: new RegExp(`${key}`, 'g'),
-			replace: `${replaceMap[key]}`
-		}));
+		$.each(data, (key, value) => {
+			
 
-		var converter = new showdown.Converter({extensions:[bindings]});
-		converter.setFlavor('github');
-
-		$.each(data, function(key, value) {
 			console.log('key: ' + key);
-			var postName = value.name;
+			var postFileName = value.name;
+			var postName = postFileName.replace(".md", "");
 			var postTime = new Date(parseInt(value.time));
 			var localTime = postTime.toLocaleString();
-			console.log('name: ' + postName);
-			console.log("locale time: " + localTime);
-			var blogPost = relative + postName;
-			$.get(blogPost, function(data) {
-				var convertedHTML = converter.makeHtml(data);
+			var bodyId = postName + '-body';
+			var buttonId = postName + '-button';
+			var contentId = postName + '-content';
+	
+			var converter = new showdown.Converter();
+			converter.setFlavor('github');
+
+			var blogPost = relative + postFileName;
+			$.get(blogPost, (markdown) => {
+				var convertedHTML = converter.makeHtml(markdown);
+				console.log(convertedHTML);
 				var html = '<div class="justify-content-center row card blog-card" id="' + postName + '"' + '>';
+				html += '<div id ="' + contentId + '">';  
 				html += convertedHTML;
+				html += '</div>'
 				html += '<p>Posted ' + localTime + '</p>';
-				html +=  '<a href="#" class="btn btn-primary btn-blog-post">Go to post</a>'
+				html +=  '<button id="' + buttonId + '" class="btn btn-primary btn-blog-post">Continue Reading</button>'
 				html += '</div>';
-				$('#blog-page').append(html);
-				console.log(data);
+
+				$('#blog-page').append(html).ready(() => {
+					var contentSection = $('#' + contentId);
+					contentSection.children().each((i, element) => {
+						$(element).filter('h1').addClass('section-header justify-content-center row');
+						if(i > 1) {
+							$(element).hide();
+						}
+					});
+					var firstPara = contentSection.find('p').first();
+					var firstPrataText = firstPara.text();
+					var truncated = truncateText(firstPrataText);
+					firstPara.text(truncated);
+
+					$('#' + buttonId).click(() => {
+						$("#" + contentId).children().each((i, element) => {
+							$(element).show();
+							firstPara.text(firstPrataText);
+						});
+					});
+				});
+				//https://getbootstrap.com/docs/4.0/components/card/#header-and-footer
 			});
 		});
 	});
 }));
+
+function isPunctuation(ch) {
+	return ch == '.' || ch == '!' || ch == '?';
+}
+
+function truncateText(text) {
+	var length = 100;
+	if(length >= text.length) {
+		length = text.length;
+	}
+	for(var i = 400; i < text.length; i++) {
+		if(isPunctuation(text[i])) {
+			length = i + 1;
+			break;
+		}	
+	}
+	return text.substring(0, length);
+}
