d1bd1bf27279b9321916232d2e028a7d1d1069c8
virustotalop <virustotalop@gmail.com>
2021-12-28 23:23:05 -0800

    Cleanup modal code
    Remove unnecessary close button code and add proper indentation

--- docs/js/modal.js
+++ docs/js/modal.js
@@ -1,5 +1,4 @@
 class Modal {
-		
 	constructor(id, title, body) {
 		this.id = id;
 		this.title = title;
@@ -7,24 +6,22 @@ class Modal {
 	}
 	
 	create(element) {
-		let html = '<div id="' + this.id + '" class="modal" tabindex="-1" role="dialog">' +
-		'<div class="modal-dialog" role="document">' +
-		'<div class="modal-content">' +
-		'<div class="modal-header">' +
-		'<h5 class="modal-title">' + this.title + '</h5>' +
-		'<button type="button" class="close" data-dismiss="modal" aria-label="Close">' +
-		'</button>' +
-		'</div>' +
-		'<div class="modal-body">' +
-		'<p>' + this.body + '</p>' +
-		'</div>' +
-		'<div class="modal-footer">' +
-		'<button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>' +
-		'</div>' +
-		'</div>' +
-		'</div>' +
+		let html = 
+		'<div id="' + this.id + '" class="modal" tabindex="-1" role="dialog">' +
+			'<div class="modal-dialog" role="document">' +
+				'<div class="modal-content">' +
+					'<div class="modal-header">' +
+						'<h5 class="modal-title">' + this.title + '</h5>' + 
+					'</div>' +
+					'<div class="modal-body">' +
+						'<p>' + this.body + '</p>' +
+					'</div>' +
+					'<div class="modal-footer">' +
+						'<button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>' +
+					'</div>' +
+				'</div>' +
+			'</div>' +
 		'</div>';
-		
 		element.innerHTML = element.innerHTML + html;
 	}
 }
