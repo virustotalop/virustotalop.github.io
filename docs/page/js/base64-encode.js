452d6505194a4dddb895ebc31c6940ffd9dc72bf
virustotalop <virustotalop@gmail.com>
2022-07-17 03:00:53 -0700

    Add base64 tools

---
+++ docs/page/js/base64-encode.js
@@ -0,0 +1,7 @@
+pageRegistrar.register(new Page("base64-encode", () => {
+    $('#encode-button').click(() => {
+        let text = $('#encode-content').val()
+        let encodedText = btoa(text)
+        $('#encoded').val(encodedText)
+    });
+}));
