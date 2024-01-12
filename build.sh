771ce1f41e3ebe6beb844a594f55c9d1eee76302
virustotalop <virustotalop@gmail.com>
2024-01-12 03:39:07 -0800

    Update docker dirs

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 docker rm zola-build
-docker run -u "$(id -u):$(id -g)" --name zola-build -v $PWD/docs_2:/docs_2 --workdir /docs_2 ghcr.io/getzola/zola:v0.17.1 build
+docker run -u "$(id -u):$(id -g)" --name zola-build -v $PWD/docs:/docs --workdir /docs ghcr.io/getzola/zola:v0.17.1 build
