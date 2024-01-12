900c39961152908b79673c532ab19f52274c70b9
virustotalop <virustotalop@gmail.com>
2024-01-12 03:36:36 -0800

    Move new website into docs

--- docs/page/js/base64-decode.js
+++
@@ -1,7 +0,0 @@
-pageRegistrar.register(new Page("base64-decode", () => {
-    $('#decode-button').click(() => {
-        let text = $('#decode-content').val()
-        let decodedText = atob(text)
-        $('#decoded').val(decodedText)
-    });
-}));
--- docs/page/js/base64-encode.js
+++
@@ -1,7 +0,0 @@
-pageRegistrar.register(new Page("base64-encode", () => {
-    $('#encode-button').click(() => {
-        let text = $('#encode-content').val()
-        let encodedText = btoa(text)
-        $('#encoded').val(encodedText)
-    });
-}));
